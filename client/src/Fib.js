c6b12e710ab187745e3aabc3e4ccfa5effc7637d
pbdavala <pavandavala@yahoo.com>
2021-05-01 11:30:25 -0400

    Comments added to files for better understanding on 05/01/2021

--- client/src/Fib.js
+++ client/src/Fib.js
@@ -1,23 +1,28 @@
 import React, { Component } from 'react';
+//axios makes requests to backend Express server 
 import axios from 'axios';
 
 class Fib extends Component {
+  //state object..
   state = {
     seenIndexes: [],
     values: {},
     index: ''
   };
 
+  //std method available 
   componentDidMount() {
     this.fetchValues();
     this.fetchIndexes();
   }
 
+  //Fetch the fib value for the current index from redisClient (server/index.js)
   async fetchValues() {
     const values = await axios.get('/api/values/current');
     this.setState({ values: values.data });
   }
 
+  //Fetch the values for all from pgClient (server/index.js)
   async fetchIndexes() {
     const seenIndexes = await axios.get('/api/values/all');
     this.setState({
@@ -26,8 +31,9 @@ class Fib extends Component {
   }
 
   handleSubmit = async event => {
+    //To makesure form from attempting to submit itself...call preventDefault()
     event.preventDefault();
-
+    //Post an object with index prop. in axios post req
     await axios.post('/api/values', {
       index: this.state.index
     });
@@ -35,9 +41,11 @@ class Fib extends Component {
   };
 
   renderSeenIndexes() {
-    return this.state.seenIndexes.map(({ number }) => number).join(', ');
+    //iterate over every object in seenIndexes array and pull & return the number. This is from Postgres
+    return this.state.seenIndexes.map(({ number }) => number).join(', ');   //Append , to the number returned.
   }
 
+  //These values are from redis with objects (index:fibValue pairs)
   renderValues() {
     const entries = [];
 
--- server/index.js
+++ server/index.js
@@ -1,9 +1,12 @@
+//keys.js in server folder will have the host name and other details
 const keys = require('./keys');
 
 // Express App Setup
 const express = require('express');
+//body-parser lib will parse incoming req. from react & turns the body of post req into json
 const bodyParser = require('body-parser');
-const cors = require('cors');
+//cors helps to make calls from one domain (react) to different domain (express)
+const cors = require('cors');     //cross version resource sharing
 
 const app = express();
 app.use(cors());
@@ -20,11 +23,14 @@ const pgClient = new Pool({
 });
 pgClient.on('error', () => console.log('Lost PG connection'));
 
+//Create a table (onetime step) for insertion of records
+//Inserts a single value -- index looked up. This the comment for .query line
 pgClient
-  .query('CREATE TABLE IF NOT EXISTS values (number INT)')
-  .catch(err => console.log(err));
+  .query('CREATE TABLE IF NOT EXISTS values (number INT)')      
+  .catch(err => console.log(err));        //Interesting syntax.
 
 // Redis Client Setup
+//Retry to connect to redis every 1 sec, if disconnected.
 const redis = require('redis');
 const redisClient = redis.createClient({
   host: keys.redisHost,
@@ -36,21 +42,27 @@ const redisPublisher = redisClient.duplicate();
 // Express route handlers
 
 app.get('/', (req, res) => {
-  res.send('Hi');
+  res.send('Hi Pavan from server/index.js');
 });
 
+//This below logic will query postgres and retrieve all values that were ever submitted to postgres.
+//async function is handling the req. here
 app.get('/values/all', async (req, res) => {
   const values = await pgClient.query('SELECT * from values');
 
   res.send(values.rows);
 });
 
+//To get the values from redis. async fn
+//Redis lib for node does not support promise. So we have to use the classic callback fn instead of await fn
+//hgetall = Look at the hash and get all it's values
 app.get('/values/current', async (req, res) => {
   redisClient.hgetall('values', (err, values) => {
     res.send(values);
   });
 });
 
+//This below will get the input submitted in the form, and stores it to postgres and redis
 app.post('/values', async (req, res) => {
   const index = req.body.index;
 
@@ -58,6 +70,7 @@ app.post('/values', async (req, res) => {
     return res.status(422).send('Index too high');
   }
 
+  //We have not yet calc. the fib of index. Hence the msg. Worker will calc. and reset it in redis hash.
   redisClient.hset('values', index, 'Nothing yet!');
   redisPublisher.publish('insert', index);
   pgClient.query('INSERT INTO values(number) VALUES($1)', [index]);
@@ -66,5 +79,5 @@ app.post('/values', async (req, res) => {
 });
 
 app.listen(5000, err => {
-  console.log('Listening');
+  console.log('Server Listening on port 5000');
 });
